df6749af51b7950b770d50e96ef308d3367f13d0
NonamedBread <lgh778923@gmail.com>
2022-04-08 19:12:10 +0900

    fix:profile

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"Blog Test Hi i`m here title : 포스트의 제목을 큰 따옴표로 적어 준다. 이 title을 적어주지 않으면 .md 파일 이름으로 적어주었던 title 부분이 제목으로 업로드 된다. excerpt : 포스트 목록에서 보여지는 블로그 소개 글로 들어가는 것 같다. categories : 이 포스트의 카테고리는 Blog로 정했다. tags : 태그와 카테고리의 차이점은 카테고리는...","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/MyFirstPost/",
+        "teaser": null
+      }]

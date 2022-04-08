d5a7f2c9fc006bdc3c44f2e36e5a334095e23ca6
NonamedBread <lgh778923@gmail.com>
2022-04-09 01:37:33 +0900

    fix:posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -14,6 +14,6 @@ var store = [{
         "title": "정처기 실기 요구사항 확인 6~10",
         "excerpt":"요구사항 정의 요구사항의 개념을 정리해 두는것이 좋음 요구사항 소프트웨어가 어떤 문제를 해결하기 위해 제공하는 서비스에 대한 설명과 운영되는데 필요한 제약 조건 요구사항의 유형 기능 요구사항(Functional requirements) 비기능 요구사항(Non-functional requirements) 사용자 요구사항(User requirements) 시스템 요구사항(System requirements) 기능 요구사항(Functional requirements) 기능이나 수행과 관련된 요구사항 비기능 요구사항(Non-functional requirements) 품질이나 제약사항과 관련된 요구사항 사용자...","categories": ["EIP_PS"],
         "tags": ["EIP_PS"],
-        "url": "/eip_ps/%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD-%ED%99%95%EC%9D%B8-01/",
+        "url": "/eip_ps/6~7%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD%ED%99%95%EC%9D%B8/",
         "teaser": null
       }]

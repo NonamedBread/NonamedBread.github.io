e2527aa2c36a92d3491b330051f23c0a10696fa2
NonamedBread <lgh778923@gmail.com>
2022-04-15 21:05:51 +0900

    fix:post-bug

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "정처기 실기 요구사항 확인 6~10",
+        "excerpt":"요구사항 정의 요구사항의 개념을 정리해 두는것이 좋음 요구사항 소프트웨어가 어떤 문제를 해결하기 위해 제공하는 서비스에 대한 설명과 운영되는데 필요한 제약 조건 요구사항의 유형 기능 요구사항(Functional requirements) 비기능 요구사항(Non-functional requirements) 사용자 요구사항(User requirements) 시스템 요구사항(System requirements) 기능 요구사항(Functional requirements) 기능이나 수행과 관련된 요구사항 비기능 요구사항(Non-functional requirements) 품질이나 제약사항과 관련된 요구사항 사용자...","categories": ["EIP_PS"],
+        "tags": ["EIP_PS"],
+        "url": "/eip_ps/6~7%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD%ED%99%95%EC%9D%B8/",
+        "teaser": null
+      },{
         "title": "[Jekyll] 블로그 포스팅하는 방법",
         "excerpt":"Blog Test Hi i`m here title : 포스트의 제목을 큰 따옴표로 적어 준다. 이 title을 적어주지 않으면 .md 파일 이름으로 적어주었던 title 부분이 제목으로 업로드 된다. excerpt : 포스트 목록에서 보여지는 블로그 소개 글로 들어가는 것 같다. categories : 이 포스트의 카테고리는 Blog로 정했다. tags : 태그와 카테고리의 차이점은 카테고리는...","categories": ["Blog"],
         "tags": ["Blog","jekyll","Github","Git"],
@@ -9,12 +15,6 @@ var store = [{
         "excerpt":"소프트웨어 생명 주기 소프트웨어 생명 주기(Software Life Cycle) 소프트웨어를 개발하기 위한 과정을 각 단계별로 나눈 것 대표 모형 폭포수 모형 프로토타입 모형 나선형 모형 애자일 모형 폭포수 모형(Waterfall Model, 원형 모형) 각 단계를 확실히 매듭짓고 다음 단계를 진행하는 개발 방법론 전통적인 소프트웨어 생명 주기 모형 프로토타입 모형(Prototype Model, 원형 모형)...","categories": ["EIP_PS"],
         "tags": ["EIP_PS"],
         "url": "/eip_ps/%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD-%ED%99%95%EC%9D%B8/",
-        "teaser": null
-      },{
-        "title": "정처기 실기 요구사항 확인 6~10",
-        "excerpt":"요구사항 정의 요구사항의 개념을 정리해 두는것이 좋음 요구사항 소프트웨어가 어떤 문제를 해결하기 위해 제공하는 서비스에 대한 설명과 운영되는데 필요한 제약 조건 요구사항의 유형 기능 요구사항(Functional requirements) 비기능 요구사항(Non-functional requirements) 사용자 요구사항(User requirements) 시스템 요구사항(System requirements) 기능 요구사항(Functional requirements) 기능이나 수행과 관련된 요구사항 비기능 요구사항(Non-functional requirements) 품질이나 제약사항과 관련된 요구사항 사용자...","categories": ["EIP_PS"],
-        "tags": ["EIP_PS"],
-        "url": "/eip_ps/6~7%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD%ED%99%95%EC%9D%B8/",
         "teaser": null
       },{
         "title": "Test",
